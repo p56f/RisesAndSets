e1735192526519c594186e9557fdc6953f4e3bdf
p56f <pawelcc@o2.pl>
2017-11-05 13:27:29 +0100

    Fix test names

--- ras-core/src/test/scala/pl/pecet/risesandsets/calculators/SunCalculatorTest.scala
+++ ras-core/src/test/scala/pl/pecet/risesandsets/calculators/SunCalculatorTest.scala
@@ -44,41 +44,41 @@ class SunCalculatorTest extends FunSuite {
     }
   }
 
-  test("Sunrise in Warsaw - 21. 06. 2017") {
+  test("Sunrise in Warsaw (21/06/2017)") {
     val sunrise = SunCalculator.calculateRise(warsawSummerParams).get
     assert(sunrise.getHour == 4)
     assert(sunrise.getMinute == 14)
     assert(sunrise.getSecond == 9)
   }
 
-  test("Sunset in Warsaw - 21. 06. 2017") {
+  test("Sunset in Warsaw (21/06/2017)") {
     val sunset = SunCalculator.calculateSet(warsawSummerParams).get
     assert(sunset.getHour == 21)
     assert(sunset.getMinute == 1)
     assert(sunset.getSecond == 5)
   }
 
-  test("Sunrise in Warsaw - 22. 12. 2017") {
+  test("Sunrise in Warsaw (22/12/2017)") {
     val sunrise = SunCalculator.calculateRise(warsawWinterParams).get
     assert(sunrise.getHour == 7)
     assert(sunrise.getMinute == 43)
     assert(sunrise.getSecond == 22)
   }
 
-  test("Sunset in Warsaw - 22. 12. 2017") {
+  test("Sunset in Warsaw (22/12/2017)") {
     val sunset = SunCalculator.calculateSet(warsawWinterParams).get
     assert(sunset.getHour == 15)
     assert(sunset.getMinute == 25)
     assert(sunset.getSecond == 27)
   }
 
-  test("Polar day on arctic circle - 21. 06. 2017") {
+  test("Polar day on arctic circle (21/06/2017)") {
     val arcticCircleSummerParams = DateAndCoordinatesParams(latitude = 66.57d, longitude = 0.0d, day = 21, month = 6, year = 2017, timeOffset = 0)
     val sunset = SunCalculator.calculateSet(arcticCircleSummerParams)
     assert(sunset.isEmpty)
   }
 
-  test("Polar night in Kiruna - 22. 12. 2017") {
+  test("Polar night in Kiruna (22/12/2017)") {
     val kirunaWinterParams = DateAndCoordinatesParams(latitude = 67.86d, longitude = 20.26d, day = 22, month = 12, year = 2017, timeOffset = 3600)
     val sunRise = SunCalculator.calculateRise(kirunaWinterParams)
     assert(sunRise.isEmpty)
