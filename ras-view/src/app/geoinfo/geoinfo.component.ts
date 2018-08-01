cc8ea5e32981ade8d22e05de7cc844f17af34c97
p56f <pawelcc@o2.pl>
2018-08-01 22:11:20 +0200

    Frontend: present sunrise and sunset

--- ras-view/src/app/geoinfo/geoinfo.component.ts
+++ ras-view/src/app/geoinfo/geoinfo.component.ts
@@ -6,6 +6,9 @@ import { GeoLocationService } from '../geolocation.service';
 import { TimeZone } from '../timezone';
 import { TimeZoneService } from '../timezone.service';
 
+import { SunInfo } from '../suninfo';
+import { SunService } from '../sun.service';
+
 @Component({
   selector: 'app-geoInfo',
   templateUrl: './geoinfo.component.html',
@@ -19,13 +22,16 @@ export class GeoInfoComponent implements OnInit {
 
   private timeZone : TimeZone;
 
+  private sunInfo : SunInfo;
+
   private currentDateTime = new Date();
   
   private timeZoneOffset = '+0000'
 
   constructor(
     private geoLocationService : GeoLocationService,
-    private timeZoneService: TimeZoneService) { }
+    private timeZoneService: TimeZoneService,
+    private sunService: SunService) { }
 
   ngOnInit() {
   }
@@ -33,10 +39,10 @@ export class GeoInfoComponent implements OnInit {
   getLocationWithTimeZone() {
     this.geoLocationService.getLocation(this.address).subscribe( data => 
       {
-        let result = data['results'][0];
-        let location = result['geometry']['location'];
+        const result = data['results'][0];
+        const location = result['geometry']['location'];
         this.geoLocation = {
-          formattedAddress : result['formatted_address'],
+          formattedAddress: result['formatted_address'],
           latitude: location['lat'],
           longitude: location['lng']
         };
@@ -52,6 +58,41 @@ export class GeoInfoComponent implements OnInit {
         this.timeZone = tz;
         this.currentDateTime = new Date();
         this.timeZoneOffset = this.getTimeZoneOffset();
+        this.getSunInfo();
+      });
+  }
+
+  private getSunInfo() {
+    this.sunService
+      .getSunInfo(this.geoLocation.latitude, this.geoLocation.longitude, this.currentDateTime, this.getTimeZoneOffsetInSeconds())
+      .subscribe( data => {
+        const polarDay = data['polarDay'];
+        const polarNight = data['polarNight'];
+        if (polarDay) {
+          this.sunInfo = {
+            sunrise: '-',
+            sunset: '-',
+            duration: 'Polar day (24h)',
+            polarDay: true,
+            polarNight: false
+          };  
+        } else if (polarNight) {
+          this.sunInfo = {
+            sunrise: '-',
+            sunset: '-',
+            duration: 'Polar night (0h)',
+            polarDay: false,
+            polarNight: true
+          };
+        } else {
+          this.sunInfo = {
+            sunrise: this.getTimeAsString(data['sunrise']),
+            sunset: this.getTimeAsString(data['sunset']),
+            duration: this.getTimeAsString(data['duration']),
+            polarDay: false,
+            polarNight: false
+          };
+        }
       });
   }
 
@@ -59,13 +100,22 @@ export class GeoInfoComponent implements OnInit {
     return Math.round(this.currentDateTime.getTime() / 1000);
   }
 
-  private getTimeZoneOffset() : string {
-    let offsetInMinutes = (this.timeZone.rawOffset + this.timeZone.dstOffset) / 60;
-    let hoursPart = Math.floor(offsetInMinutes / 60);
-    let hoursPartString = `${Math.abs(hoursPart)}`.padStart(2, '0');
-    let minutesPartString = `${offsetInMinutes % 60}`.padStart(2, '0');
+  private getTimeZoneOffsetInSeconds() : number {
+    return this.timeZone.rawOffset + this.timeZone.dstOffset
+  }
 
+  private getTimeZoneOffset() : string {
+    const offsetInMinutes = this.getTimeZoneOffsetInSeconds() / 60;
+    const hoursPart = Math.floor(offsetInMinutes / 60);
+    const hoursPartString = `${Math.abs(hoursPart)}`.padStart(2, '0');
+    const minutesPartString = `${offsetInMinutes % 60}`.padStart(2, '0');
     return `${(hoursPart >= 0) ? '+' : '-'}${hoursPartString}${minutesPartString}`;
   }
 
+  private getTimeAsString(time: any) : string {
+    const hour = `${time['hour']}`.padStart(2, '0')
+    const minute = `${time['minute']}`.padStart(2, '0')
+    const second = `${time['second']}`.padStart(2, '0')
+    return `${hour}:${minute}:${second}`
+  }
 }
--- ras-view/src/app/sun.service.ts
+++ ras-view/src/app/sun.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SunService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:12800/sun';
+
+  constructor(private http : HttpClient) {}
+  
+  getSunInfo(latitude: number, longitude: number, date: Date, timeOffset: number) : Observable<any> {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    const url = `${this.baseUrl}?latitude=${latitude}&longitude=${longitude}` + 
+            `&day=${day}&month=${month}&year=${year}&timeOffset=${timeOffset}`;
+    return this.http.get(url);
+  }
 }
