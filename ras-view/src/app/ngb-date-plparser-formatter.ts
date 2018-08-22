92c5bfe3678346c65e59868cc6ac9d7bd13328f2
p56f <pawelcc@o2.pl>
2018-08-22 17:39:17 +0200

    Use moment.js to format date in datepicker

--- ras-view/src/app/ngb-date-plparser-formatter.ts
+++ ras-view/src/app/ngb-date-plparser-formatter.ts
@@ -1,46 +1,37 @@
 import { Injectable } from "@angular/core";
 import { NgbDateParserFormatter, NgbDateStruct } from "@ng-bootstrap/ng-bootstrap";
 
+import * as moment from 'moment';
+
 @Injectable()
 export class NgbDatePLParserFormatter extends NgbDateParserFormatter {
+    private readonly dateFormat = 'DD.MM.YYYY';
+
     parse(value: string): NgbDateStruct {
         if (value) {
-            const dateParts = value.split('.');
-            if (dateParts.length !== 3) {
-                return null;
+            const dateAsMoment = moment(value, this.dateFormat);
+            if (dateAsMoment.isValid()) { 
+                return {
+                    day: dateAsMoment.date(),
+                    month: dateAsMoment.month() + 1,
+                    year: dateAsMoment.year()
+                };
             }
-            return this.convertToDate(dateParts);
+            return null;
         }
         return null;
     }    
     
     format(date: NgbDateStruct): string {
       if (date != null) {
-        const day = date.day.toString().padStart(2, '0');
-        const month = date.month.toString().padStart(2, '0');
-        const year = date.year.toString();
-        return `${day}.${month}.${year}`;
+        const dateAsMoment = moment({
+            date: date.day,
+            month: date.month - 1,
+            year: date.year
+        });
+        return dateAsMoment.format(this.dateFormat);
       }
 
       return null;
-    }
-
-    private convertToDate(dateParts: string[]) : NgbDateStruct {
-        const day = parseInt(dateParts[0]);
-        const month = parseInt(dateParts[1]);
-        const year = parseInt(dateParts[2]);
-        if (!isNaN(day) && !isNaN(month) && !isNaN(year) && this.isCorrectDate(day, month, year)) {
-            return {day: day, month: month, year: year};
-        }
-        return null;
-    }
-
-    private isCorrectDate(day: number, month: number, year: number) : boolean {
-        const daysInMonth = [31, this.daysInFebruary(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-        return (month > 0) && (month <= 12) && (day <= daysInMonth[month - 1]); 
-    }
-
-    private daysInFebruary(year: number) : number {
-        return (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) ? 29 : 28;
     }
 }
